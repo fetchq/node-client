4ac68858a776e4579ad646db03bb6e11a4e0313b
marcopeg <marco.pegoraro@gmail.com>
2021-02-20 14:12:08 +0000

    Add hello world example

---
+++ examples/hello-world/index.js
@@ -0,0 +1,39 @@
+const fetchq = require('fetchq');
+const DATABASE_URL = 'postgres://postgres:postgres@localhost:5432/postgres';
+
+console.log('');
+console.log('###');
+console.log('### FetchQ Client // Examples // Hello World');
+console.log('###');
+console.log('');
+
+const client = fetchq({
+  clientName: 'c1',
+  logLevel: 'info',
+  connectionString: process.env.DATABASE_URL || DATABASE_URL,
+  queues: [
+    {
+      name: 'q1',
+      workerHandler: (doc, { client }) => {
+        client.logger.info(` Working on: ${doc.subject.split('-').shift()}...`);
+        return doc.complete();
+      },
+    },
+  ],
+});
+
+console.log('###');
+console.log('### connecting to: ');
+console.log('###', client.settings.connectionString);
+console.log('###');
+console.log('');
+
+client
+  .boot()
+  .then((client) => {
+    setInterval(() => client.doc.append('q1'), 1000);
+  })
+  .catch((err) => {
+    console.log('Failed boot');
+    console.error(err);
+  });
