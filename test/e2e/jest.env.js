5292d36830f8e48a518b2dd3d4bfacdf15f63825
marcopeg <marco.pegoraro@gmail.com>
2021-02-20 16:53:31 +0000

    Updated depencencies

--- test/e2e/jest.env.js
+++ test/e2e/jest.env.js
@@ -3,9 +3,12 @@ const envalid = require('envalid');
 // const PGSTRING = 'postgresql://gitpod:gitpod@localhost:5432/postgres';
 const PGSTRING = 'postgresql://postgres:postgres@localhost:5432/postgres';
 
-module.exports = () =>
-  envalid.cleanEnv(process.env, {
+module.exports = () => {
+  const env = envalid.cleanEnv(process.env, {
     PGSTRING: envalid.url({
       default: PGSTRING,
     }),
   });
+
+  return { ...env };
+};
